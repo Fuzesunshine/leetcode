06b4eccf0e961c9af0e7085c614ae4cdc7d74e81
Fuze <178880457@qq.com>
2022-07-24 11:28:57 +0800

    Time: 5806 ms (19.99%), Space: 31.7 MB (90.56%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,11 +1,26 @@
-from sortedcontainers import SortedList
-
 class Solution(object):
     def countSmaller(self, nums):
-        res = []
-        sorted_nums = SortedList(nums)
-        for e in nums:
-            idx = sorted_nums.index(e)
-            res.append(idx)
-            sorted_nums.remove(e)
-        return res
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        
+        def binarySearch(sort_list, val):
+            l = 0
+            r = len(sort_list) - 1
+            while l < r:
+                mid = (l+r) // 2
+                if sort_list[mid] < val:
+                    l = mid + 1
+                else:
+                    r = mid
+            return l
+        
+        sort_nums = sorted(nums)
+        ans = []
+        for num in nums:
+            pos = binarySearch(sort_nums, num)
+            sort_nums.pop(pos)
+            ans.append(pos)
+        
+        return ans
