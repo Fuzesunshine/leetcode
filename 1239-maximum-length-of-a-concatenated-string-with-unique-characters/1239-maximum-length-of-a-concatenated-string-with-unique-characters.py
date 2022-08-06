1810da70bf2e5f004898528630b2c05a2428fe5e
Fuze <178880457@qq.com>
2022-08-06 09:03:42 +0800

    Time: 125 ms (84.34%), Space: 13.7 MB (57.83%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -18,14 +18,10 @@ class Solution(object):
                 return
             for i in range(start + 1, len(arr)):
                 if not (set(tmp) & set(arr[i])):
-                    visited.add(arr[i])
                     dfs(i, tmp + arr[i])
-                    visited.remove(arr[i])
         
         for i, s in enumerate(arr):
-            visited.add(s)
             dfs(i, s)
-            visited.remove(s)
         
         return self.res
                 
